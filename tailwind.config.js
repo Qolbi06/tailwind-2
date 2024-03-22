7d292939f6c603ea3f4565d188f5ea383351e6b6
Luthfi Qolbi <luthfiqolbi@gmail.com>
2024-03-22 11:03:36 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", 
+  content: ["./src/**/*{html,js}", 
   "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
-
+  plugins: [require("flowbite/plugin")],
+};
